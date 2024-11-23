e00fbe40223fbe82291f7b5fcd7dd63669627467
ajay-addu <panduajay098@gmail.com>
2024-11-23 16:33:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 2020;
+const PORT = process.env.PORT || 2029;
 
 app.get('/', (req, res) => {
     res.send('Hello, Docker!');
 });
 
-app.listen(2021, '0.0.0.0', () => {
-    console.log('Server is running on port 2020');
+app.listen(2029, '0.0.0.0', () => {
+    console.log('Server is running on port 2029');
 });
